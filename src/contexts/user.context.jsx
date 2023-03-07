89c58d74a65310480576ead0d9fca2d2704bcd6a
dicey <esanelijah@icloud.com>
2023-03-07 06:41:29 -0800

    observer pattern

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,13 +1,28 @@
-import { createContext, useState } from "react";
+import { createContext, useState , useEffect } from "react";
+ 
+import { onAuthStateChangedListener ,signOutUser } from "../utils/firebase/firebase.utils";
 
 export const UserContext = createContext({
   setCurrentUser: () => null,
   currentUser: null,
 });
 
+
 export const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
   const value = { currentUser, setCurrentUser };
 
+
+  signOutUser()
+
+  useEffect( () => {
+    const unsubcribe = onAuthStateChangedListener((user) => {
+      setCurrentUser(user)
+    })
+  
+    return unsubcribe
+  } , [])
+
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
+
