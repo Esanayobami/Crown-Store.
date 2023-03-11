ec1432f3fd4ad52a6c87949f0e42ad989882d418
dicey <esanelijah@icloud.com>
2023-03-11 06:00:14 -0800

    shop

--- src/routes/shop/Shop-component.jsx
+++ src/routes/shop/Shop-component.jsx
@@ -1,20 +1,15 @@
 import { ProductsContext } from "../../contexts/product.context";
 import { useContext } from "react";
+import ProductCard from "../../components/products/ProductsCard.component";
 
 const Shop = () => {
 
     const { products } = useContext(ProductsContext);
     return ( 
         <div>
-            {
-                products.map(({ id , name}) => {
-                    return (
-                        <div key={id}>
-                            <h1>{name}</h1>
-                        </div>
-                    )
-                })
-            }
+        {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
         </div>
      )
 }
